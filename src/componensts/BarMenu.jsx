b85ff593741eb0b2fe1248cd32d1be551c20524b
Takia Akter Mukta <mukta2169@gmail.com>
2024-06-03 00:32:41 +0600

    BarMenu added

--- src/componensts/BarMenu.jsx
+++ src/componensts/BarMenu.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
-
+import Container from './layer/Container'
+import Image from './layer/Image'
+import bg from '../assets/glass.jpg'
 const BarMenu = () => {
   return (
-    <div>
-      Bar Menu
+  
+    <div className='font-CU'>
+         <Image className='absolute w-full' src={bg}/>
+
+        <Container >
+      
+        <div className=' text-white absolute text-[64px] pt-40'>
+        <h3 className='border-b-4 border-white max-w-[242px]' >Bar Menu</h3>
+        <h3 className='py-[65px]'>Food Menu</h3>
+        <h3>Desserts Menu</h3>
+        </div>
+        </Container>
     </div>
   )
 }
