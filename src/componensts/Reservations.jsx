02e13a76fb378fa98e7233b5e83fbe08693f68fd
Omor Riduan <93811768+smRid@users.noreply.github.com>
2024-06-02 22:31:32 +0600

    Reservations added

---
+++ src/componensts/Reservations.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import Container from './layer/Container'
+import Image from './layer/Image'
+import spone from '../assets/spone.png'
+import Button from './layer/Button'
+
+
+const Reservations = () => {
+  return (
+    <div className='h-[959px] bg-[#0C0B08] flex justify-center items-center ' >
+      <Container>
+        <div className='w-[1364px] bg-[#0C0C0C] border text-center py-12 px-8 '>
+            <div className='header  m-auto  '>
+              <p className='text-[#FFFFFF] font-CU text-[23px] font-bold leading-[30px]'>Reservations</p>
+              <p className='mt-2 flex justify-center'><Image src={spone} alt={"spone"} /></p>
+              <p className='text-[#DCCA87] font-CU text-[64px] font-semibold leading-[83px] capitalize'>Book A Table</p>
+            </div>
+
+
+            <div className='my-16 flex gap-x-8 justify-center'>
+              
+            <div className='border bg-transparent border-[#AAAAAA] text-[#767676] lg:text-sm text-xs w-[412px]'>
+              
+              <select className='outline-none text-[#AAAAAA] my-4 ml-4 flex justify-start bg-transparent'>
+
+                        <option className=' bg-[#0C0B08]' value="SIZE">
+                         1 person</option>
+
+                         <option className=' bg-[#0C0B08]'value="Featured">
+                         2 person</option>
+
+                         <option className=' bg-[#0C0B08]'value="Featured">
+                         3 person</option>
+
+                </select>
+            </div>
+
+            <div className='border bg-transparent border-[#AAAAAA] text-[#767676] lg:text-sm text-xs w-[412px]'>
+              
+              <select className='outline-none text-[#AAAAAA] my-4 ml-4 flex justify-start bg-transparent'>
+
+                        <option className=' bg-[#0C0B08]' value="SIZE">
+                        12/05/2021</option>
+
+                         
+
+                </select>
+            </div>
+
+            <div className='border bg-transparent border-[#AAAAAA] text-[#767676] lg:text-sm text-xs w-[412px]'>
+              
+              <select className='outline-none text-[#AAAAAA] my-4 ml-4 flex justify-start bg-transparent'>
+
+                        <option className=' bg-[#0C0B08]' value="SIZE">
+                        11:00 AM</option>
+
+                        
+
+                </select>
+            </div>
+             
+            </div>
+            <Button className= 'w-[133px]' text="Book Now"/>
+        </div>
+      </Container>
+    </div>
+  )
+}
+
+export default Reservations
--- src/componensts/page/Home.jsx
+++ src/componensts/page/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Banner from '../Banner'
 import bg from "../../assets/bg.jpg"
 import About from '../About'
+import Reservations from '../Reservations'
 
 const Home = () => {
   return (
     <div className='bg-bg-img bg-cover'>
       <Banner/>
       <About/>
+      <Reservations/>
     </div>
   )
 }
