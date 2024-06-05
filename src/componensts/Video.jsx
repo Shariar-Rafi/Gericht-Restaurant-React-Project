4ae5da16a409f240e0d31538706f1573634ab18e
Shariar Rafi <shariar.rafi364@gmail.com>
2024-06-05 23:40:31 +0600

    updated

--- src/componensts/Video.jsx
+++ src/componensts/Video.jsx
@@ -1,19 +1,12 @@
 import React from 'react'
-import vid01 from "../Videos/restaurant.mp4"
-import thumbnail from "../assets/thumbnail.jpg"
+import vid from "../Videos/restaurant.mp4"
+// import thumbnail from "../assets/thumbnail.jpg"
 
-import { Player } from 'video-react'
 const Video = () => {
     return (
 
-        <div className='!p-0 !m-0'>
-            <Player
-            playsInline
-                poster={thumbnail}
-                
-            >
-                <source src={vid01} />
-            </Player>
+        <div className='w-full'>
+            <video src={vid}  autoPlay="true"/>
         </div>
     )
 }
