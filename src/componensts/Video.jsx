5eead6c5e7373a7d7a5be70fb3aeb56817349de7
Shariar Rafi <shariar.rafi364@gmail.com>
2024-06-08 20:58:38 +0600

    video component updated

--- src/componensts/Video.jsx
+++ src/componensts/Video.jsx
@@ -1,14 +1,9 @@
 import vid02 from "../video/restaurant.mp4"
 import ReactPlayer from 'react-player';
 import React, { useRef } from 'react';
-import thumbnail01 from "../assets/thumbnail01.jpg"
-import Image from "./layer/Image";
-
 
 const Video = () => {
     const playerRef = useRef(null);
-    const pic = <Image className="w-full" src={thumbnail01} />
-
 
     return (
 
@@ -18,8 +13,8 @@ const Video = () => {
                 height="100%"
                 playing={true}
                 muted={true}
-                light= {pic}
-                ref={playerRef} url={vid02} controls={true} />
+                ref={playerRef} url={vid02} 
+                controls={false} />
         </div>
     )
 }
