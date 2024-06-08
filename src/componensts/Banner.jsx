130ac44d4bc560a69a9b9445c3e639e49c167dc1
Shariar Rafi <shariar.rafi364@gmail.com>
2024-06-08 18:35:44 +0600

    video and banner component updated

--- src/componensts/Banner.jsx
+++ src/componensts/Banner.jsx
@@ -6,48 +6,97 @@ import fork from '../assets/fork.png'
 import banner01 from '../assets/banner01.jpg'
 import Button from './layer/Button'
 import { Link } from 'react-router-dom'
+import Slider from "react-slick";
 
 const Banner = () => {
+
+  var settings = {
+    dots: true,
+    arrows: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    appendDots: dots => (
+      <div
+        style={{
+          backgroundColor: "black",
+          padding: "",
+        }}
+      >
+        <ul className="flex mx-auto justify-center items-center gap-x-2 mt-[64px]"> {dots} </ul>
+      </div>
+    ),
+    customPaging: i => (
+      <div
+        className='w-2 text-white font-OS text-base font-semibold leading-7 capitalize'
+      >
+        {i + 1}
+      </div>
+    )
+  };
   return (
-    <div className='bg-[#0C0C0C] pb-[152px]'>
-      <Container  className="relative pt-12 pb-[92px] bg-[#0C0C0C]">
-        <p className='-rotate-90 text-white font-OS text-lg leading-8 inline-block my-auto absolute top-1/2'>
-            <Link>#Gericht</Link>
+    <div className='bg-[#FFF] pb-[152px]'>
+      <Container className="relative pt-12 pb-[92px] bg-[#0C0C0C]">
+        <p className='-rotate-90 text-white font-OS text-lg leading-8 inline-block my-auto absolute top-1/2 z-50'>
+          <Link to="/">#Gericht</Link>
         </p>
-        <div className='h-[736px] flex  '>
-            <div className="left flex flex-col w-[453px] my-auto ml-[158px] mr-[180px]">
-                <p className='text-[23px] text-[#FFF] font-CU leading-7 tracking-[0.92px] capitalize font-bold'>Chase the new Flavour</p>
-                <p className='mt-2 flex gap-x-2'>
-                  <Image src={fork} alt={"fork"} />
-                  <Image src={spoon} alt={"spoon"} />
-                </p>
-                <h2  className='text-[90px] text-[#DCCA87] font-CU leading-[117px]  capitalize font-bold '>
-                The key to Fine dining
-                </h2>
-                <p className='text-base text-[#AAA] leading-7 font-OS my-8'>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
-                <Button text="Explore Menu"/>
+
+
+
+
+
+
+        {/* -------------------------------banner-------------------------------- */}
+        {/* <div className='h-[736px] flex  '>
+          <div className="left flex flex-col w-[453px] my-auto ml-[158px] mr-[180px]">
+            <p className='text-[23px] text-[#FFF] font-CU leading-7 tracking-[0.92px] capitalize font-bold'>Chase the new Flavour</p>
+            <p className='mt-2 flex gap-x-2'>
+              <Image src={fork} alt={"fork"} />
+              <Image src={spoon} alt={"spoon"} />
+            </p>
+            <h2 className='text-[90px] text-[#DCCA87] font-CU leading-[117px]  capitalize font-bold '>
+              The key to Fine dining
+            </h2>
+            <p className='text-base text-[#AAA] leading-7 font-OS my-8'>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
+            <Button text="Explore Menu" />
+
+          </div>
+
+
+
+
+          <div className="right w-[667px] relative">
+            <div className="top size-[435px] bg-[#DCCA87] absolute right-0">
+
+            </div>
+            <div>
+              <Image src={banner01} className="z-20 absolute top-6 left-6" />
+            </div>
+            <div className="bottom size-[435px] bg-[#DCCA87] absolute left-0 bottom-0">
 
             </div>
 
+          </div>
+        </div> */}
+        {/* -------------------------------banner-------------------------------- */}
+
+        <Slider {...settings}>
+          <div className='py-12 bg-slate-500 text-center'>
+            <h3>1</h3>
+          </div>
+          <div className='py-12 bg-slate-400 text-center'>
+            <h3>2</h3>
+          </div>
+          <div className='py-12 bg-slate-300 text-center'>
+            <h3>3</h3>
+          </div>
+        </Slider>
 
 
 
-            <div className="right w-[667px] relative">
-                <div className="top size-[435px] bg-[#DCCA87] absolute right-0">
 
-                </div>
-                <div>
-                <Image src={banner01} className="z-20 absolute top-6 left-6" />
-                </div>
-                <div className="bottom size-[435px] bg-[#DCCA87] absolute left-0 bottom-0">
-
-                </div>
-                
-            </div>
-        </div>
-
-
-        <p className='text-white text-center mt-[64px]'>1-2 3 4</p>
+        {/* <p className='text-white text-center mt-[64px]'>1-2 3 4</p> */}
       </Container>
     </div>
   )
--- src/componensts/Video.jsx
+++ src/componensts/Video.jsx
@@ -1,10 +1,15 @@
 import vid02 from "../video/restaurant.mp4"
 import ReactPlayer from 'react-player';
 import React, { useRef } from 'react';
+import thumbnail01 from "../assets/thumbnail01.jpg"
+import Image from "./layer/Image";
 
 
 const Video = () => {
     const playerRef = useRef(null);
+    const pic = <Image className="w-full" src={thumbnail01} />
+
+
     return (
 
         <div className='w-full object-cover'>
@@ -13,6 +18,7 @@ const Video = () => {
                 height="100%"
                 playing={true}
                 muted={true}
+                light= {pic}
                 ref={playerRef} url={vid02} controls={true} />
         </div>
     )
