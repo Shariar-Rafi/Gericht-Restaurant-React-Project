a79734317d2eda00cd3cbc21cea41bdc515f35bf
Omor Riduan <93811768+smRid@users.noreply.github.com>
2024-06-21 00:58:11 +0600

    Blogs slider added

--- src/componensts/Blogs.jsx
+++ src/componensts/Blogs.jsx
@@ -8,9 +8,129 @@ import Blog2 from '../assets/Blog2.jpg'
 import Blog3 from '../assets/Blog3.jpg'
 import { Link } from 'react-router-dom'
 import Button from './layer/Button'
+import Slider from 'react-slick'
+import { FaArrowRightLong,FaArrowLeftLong } from "react-icons/fa6";
+
+
+function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className='w-16 h-16 absolute right-5 top-1/2 translate-y-[-50%] rounded-full border !flex justify-center items-center cursor-pointer'
+      style={{ ...style, display: "block", }}
+      onClick={onClick}
+    >
+      <FaArrowRightLong className='text-white'/>
+
+    </div>
+  );
+}
+
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+    className='w-16 h-16 absolute left-5 top-1/2 translate-y-[-50%] rounded-full border !flex justify-center items-center z-50 cursor-pointer'
+    style={{ ...style, display: "block", }}
+    onClick={onClick}
+  >
+    <FaArrowLeftLong className='text-white'/>
+
+  </div>
+  );
+}
+
+function SampleNextArrow2(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className='lg:w-16 lg:h-16 w-10 h-10 absolute lg:right-5 right-1 top-1/2 translate-y-[-50%] rounded-full bg-[#c3c3c2] !flex justify-center items-center cursor-pointer'
+      style={{ ...style, display: "block", }}
+      onClick={onClick}
+    >
+      <FaArrowRightLong className='text-white'/>
+
+    </div>
+  );
+}
+
+function SamplePrevArrow2(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+    className='lg:w-16 lg:h-16 w-10 h-10 absolute lg:left-5 left-1 top-1/2 translate-y-[-50%] rounded-full bg-[#c3c3c2] !flex justify-center items-center z-50 cursor-pointer'
+    style={{ ...style, display: "block", }}
+    onClick={onClick}
+  >
+    <FaArrowLeftLong className='text-white'/>
+
+  </div>
+  );
+}
+
+
 
 
 const Blogs = () => {
+
+
+  
+  var settings = {
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: false,
+    autoplay: true,
+    speed: 1000,
+    autoplaySpeed: 6000,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+
+    responsive: [
+      {
+        breakpoint: 1080,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 750,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },    {
+        breakpoint: 700,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }, 
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+
+          nextArrow: <SampleNextArrow2 />,
+          prevArrow: <SamplePrevArrow2 />,
+
+        }
+      }
+    ]
+
+
+  };
+
+
   return (
     <div className='py-[152px] '>
       <Container>
@@ -29,74 +149,130 @@ const Blogs = () => {
 
 
 
-        <div className='main flex justify-center items-center gap-x-8'>
-        <div className='group'>
-        <Image className='cursor-auto' src={Blog1}/>
-        <div className='flex justify-between w-[410px] mt-6'>
-          <p className='text-[#FAFAFA] font-OS text-[16px] leading-7 '>16 Apr 2021</p>
-          <p className='text-[#FAFAFA] font-OS text-[16px] leading-7'>- Annalisa L</p>
-        </div>
-        <div className='w-[412px] '>
-            {/* <div className='group'> */}
+        {/* <div className='main flex justify-center items-center gap-x-8'> */}
 
-                <p className='text-[#DCCA87] font-CU text-[32px] leading-[42px] capitalize
-                mt-8 tracking-[1.28px] relative group inline-block'>tips for prepping and caring<span className="absolute -bottom-[0.5px] left-0 w-0 h-[1px] bg-[#DCCA87] transition-all duration-300 group-hover:w-full"></span></p>
-                <p className='text-[#DCCA87] font-CU text-[32px] leading-[42px] capitalize tracking-[1.28px] relative group inline-block'>
-                for your grill
-                <span className="absolute bottom-0 left-0 w-0 h-[1px] bg-[#DCCA87] transition-all duration-700 group-hover:w-full"></span>
-                </p>
-            {/* </div> */}
+            
+<Slider {...settings}>
 
-        <p className='font-OS text-[#AAA] mt-8 mb-8 leading-7 tracking-[0.64px] text-base'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mus in.</p>
+    <div className='group'>
+      <Image className='cursor-auto' src={Blog1}/>
+      <div className='flex justify-between w-[410px] mt-6'>
+        <p className='text-[#FAFAFA] font-OS text-[16px] leading-7 '>16 Apr 2021</p>
+        <p className='text-[#FAFAFA] font-OS text-[16px] leading-7'>- Annalisa L</p>
       </div>
+      <div className='w-[412px] '>
+          {/* <div className='group'> */}
 
-      <Link className='text-[#DCCA87] font-CU text-base leading-7 font-bold tracking-[0.64px]' >Read More</Link>
-        
+              <p className='text-[#DCCA87] font-CU text-[32px] leading-[42px] capitalize
+              mt-8 tracking-[1.28px] relative group inline-block'>tips for prepping and caring<span className="absolute -bottom-[0.5px] left-0 w-0 h-[1px] bg-[#DCCA87] transition-all duration-300 group-hover:w-full"></span></p>
+              <p className='text-[#DCCA87] font-CU text-[32px] leading-[42px] capitalize tracking-[1.28px] relative group inline-block'>
+              for your grill
+              <span className="absolute bottom-0 left-0 w-0 h-[1px] bg-[#DCCA87] transition-all duration-700 group-hover:w-full"></span>
+              </p>
+          {/* </div> */}
 
+      <p className='font-OS text-[#AAA] mt-8 mb-8 leading-7 tracking-[0.64px] text-base'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mus in.</p>
+    </div>
+
+    <Link className='text-[#DCCA87] font-CU text-base leading-7 font-bold tracking-[0.64px]' >Read More</Link>
+      
+
+    </div>
+
+
+    <div className='group'>
+      <Image className='cursor-auto' src={Blog2}/>
+      <div className='flex justify-between w-[410px] mt-6'>
+        <p className='text-[#FAFAFA] font-OS text-[16px] leading-7 '>23 May 2021</p>
+        <p className='text-[#FAFAFA] font-OS text-[16px] leading-7'>-John Micheal</p>
       </div>
+      <div className='w-[412px]'>
 
-      <div className='group'>
-        <Image className='cursor-auto' src={Blog2}/>
+              <p className='text-[#DCCA87]  font-CU text-[32px] leading-[42px] capitalize
+              mt-8 tracking-[1.28px] relative group inline-block'>summer cocktails and <span className="absolute -bottom-[0.5px] left-0 w-0 h-[1px] bg-[#DCCA87] transition-all duration-300 group-hover:w-full"></span></p>
+              <p className='text-[#DCCA87]  font-CU text-[32px] leading-[42px] capitalize
+              tracking-[1.28px] relative group inline-block'>mocktails<span className="absolute -bottom-[0.5px] left-0 w-0 h-[1px] bg-[#DCCA87] transition-all duration-700 group-hover:w-full"></span></p>
+
+      <p className='font-OS text-[#AAA] mt-8 mb-8 leading-7 tracking-[0.64px] text-base'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mus in.</p>
+    </div>
+
+    <Link className='text-[#DCCA87] font-CU text-base leading-7 font-bold tracking-[0.64px]' >Read More</Link>
+      
+
+    </div>
+
+
+    <div className='group'>
+        <Image className='cursor-auto' src={Blog3}/>
         <div className='flex justify-between w-[410px] mt-6'>
-          <p className='text-[#FAFAFA] font-OS text-[16px] leading-7 '>23 May 2021</p>
-          <p className='text-[#FAFAFA] font-OS text-[16px] leading-7'>-John Micheal</p>
+          <p className='text-[#FAFAFA] font-OS text-[16px] leading-7 '>06 Aug 2021</p>
+          <p className='text-[#FAFAFA] font-OS text-[16px] leading-7'>-Fred W</p>
         </div>
         <div className='w-[412px]'>
 
-                <p className='text-[#DCCA87]  font-CU text-[32px] leading-[42px] capitalize
-                mt-8 tracking-[1.28px] relative group inline-block'>summer cocktails and <span className="absolute -bottom-[0.5px] left-0 w-0 h-[1px] bg-[#DCCA87] transition-all duration-300 group-hover:w-full"></span></p>
-                <p className='text-[#DCCA87]  font-CU text-[32px] leading-[42px] capitalize
-                tracking-[1.28px] relative group inline-block'>mocktails<span className="absolute -bottom-[0.5px] left-0 w-0 h-[1px] bg-[#DCCA87] transition-all duration-700 group-hover:w-full"></span></p>
+              <p className='text-[#DCCA87]  font-CU text-[32px] leading-[42px] capitalize
+              mt-8 tracking-[1.28px] relative group inline-block'>easy cooking for college<span className="absolute -bottom-[0.5px] left-0 w-0 h-[1px] bg-[#DCCA87] transition-all duration-300 group-hover:w-full"></span></p>
+              <p className='text-[#DCCA87]  font-CU text-[32px] leading-[42px] capitalize
+              tracking-[1.28px] relative group inline-block'>students<span className="absolute -bottom-[0.5px] left-0 w-0 h-[1px] bg-[#DCCA87] transition-all duration-700 group-hover:w-full"></span></p>
 
-        <p className='font-OS text-[#AAA] mt-8 mb-8 leading-7 tracking-[0.64px] text-base'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mus in.</p>
-      </div>
+      <p className='font-OS text-[#AAA] mt-8 mb-8 leading-7 tracking-[0.64px] text-base'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mus in.</p>
+    </div>
 
       <Link className='text-[#DCCA87] font-CU text-base leading-7 font-bold tracking-[0.64px]' >Read More</Link>
         
 
       </div>
 
-        <div className='group'>
-          <Image className='cursor-auto' src={Blog3}/>
-          <div className='flex justify-between w-[410px] mt-6'>
-            <p className='text-[#FAFAFA] font-OS text-[16px] leading-7 '>06 Aug 2021</p>
-            <p className='text-[#FAFAFA] font-OS text-[16px] leading-7'>-Fred W</p>
-          </div>
-          <div className='w-[412px]'>
+    <div className='group'>
+    <Image className='cursor-auto' src={Blog1}/>
+    <div className='flex justify-between w-[410px] mt-6'>
+      <p className='text-[#FAFAFA] font-OS text-[16px] leading-7 '>16 Apr 2021</p>
+      <p className='text-[#FAFAFA] font-OS text-[16px] leading-7'>- Annalisa L</p>
+    </div>
+    <div className='w-[412px] '>
+        {/* <div className='group'> */}
 
-                <p className='text-[#DCCA87]  font-CU text-[32px] leading-[42px] capitalize
-                mt-8 tracking-[1.28px] relative group inline-block'>easy cooking for college<span className="absolute -bottom-[0.5px] left-0 w-0 h-[1px] bg-[#DCCA87] transition-all duration-300 group-hover:w-full"></span></p>
-                <p className='text-[#DCCA87]  font-CU text-[32px] leading-[42px] capitalize
-                tracking-[1.28px] relative group inline-block'>students<span className="absolute -bottom-[0.5px] left-0 w-0 h-[1px] bg-[#DCCA87] transition-all duration-700 group-hover:w-full"></span></p>
+            <p className='text-[#DCCA87] font-CU text-[32px] leading-[42px] capitalize
+            mt-8 tracking-[1.28px] relative group inline-block'>tips for prepping and caring<span className="absolute -bottom-[0.5px] left-0 w-0 h-[1px] bg-[#DCCA87] transition-all duration-300 group-hover:w-full"></span></p>
+            <p className='text-[#DCCA87] font-CU text-[32px] leading-[42px] capitalize tracking-[1.28px] relative group inline-block'>
+            for your grill
+            <span className="absolute bottom-0 left-0 w-0 h-[1px] bg-[#DCCA87] transition-all duration-700 group-hover:w-full"></span>
+            </p>
+        {/* </div> */}
 
-        <p className='font-OS text-[#AAA] mt-8 mb-8 leading-7 tracking-[0.64px] text-base'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mus in.</p>
-      </div>
+    <p className='font-OS text-[#AAA] mt-8 mb-8 leading-7 tracking-[0.64px] text-base'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mus in.</p>
+  </div>
 
-        <Link className='text-[#DCCA87] font-CU text-base leading-7 font-bold tracking-[0.64px]' >Read More</Link>
-          
+  <Link className='text-[#DCCA87] font-CU text-base leading-7 font-bold tracking-[0.64px]' >Read More</Link>
+    
 
-        </div>
-        </div>
+  </div>
+
+  <div className='group'>
+    <Image className='cursor-auto' src={Blog2}/>
+    <div className='flex justify-between w-[410px] mt-6'>
+      <p className='text-[#FAFAFA] font-OS text-[16px] leading-7 '>23 May 2021</p>
+      <p className='text-[#FAFAFA] font-OS text-[16px] leading-7'>-John Micheal</p>
+    </div>
+    <div className='w-[412px]'>
+
+            <p className='text-[#DCCA87]  font-CU text-[32px] leading-[42px] capitalize
+            mt-8 tracking-[1.28px] relative group inline-block'>summer cocktails and <span className="absolute -bottom-[0.5px] left-0 w-0 h-[1px] bg-[#DCCA87] transition-all duration-300 group-hover:w-full"></span></p>
+            <p className='text-[#DCCA87]  font-CU text-[32px] leading-[42px] capitalize
+            tracking-[1.28px] relative group inline-block'>mocktails<span className="absolute -bottom-[0.5px] left-0 w-0 h-[1px] bg-[#DCCA87] transition-all duration-700 group-hover:w-full"></span></p>
+
+    <p className='font-OS text-[#AAA] mt-8 mb-8 leading-7 tracking-[0.64px] text-base'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mus in.</p>
+  </div>
+
+  <Link className='text-[#DCCA87] font-CU text-base leading-7 font-bold tracking-[0.64px]' >Read More</Link>
+    
+
+  </div>
+
+
+</Slider>
+     
+        {/* </div> */}
 
 
 
